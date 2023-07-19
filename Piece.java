56d506bd879d0c9f0a07e35c8eaa3ae45ee63fcd
ShubhamK-Yadav <syadav2098@gmail.com>
2023-07-19 23:05:54 +0100

    Getting started with implementing internal logic system and GUI interface seperately (currently it's all as one)

--- Piece.java
+++ Piece.java
@@ -16,7 +16,31 @@ public class Piece{
     public static final int TYPE_KING = 5;
     public static final int TYPE_PAWN = 6;
 
+    //Chess is played on a square board of
+    //eight rows (denoted with numbers 1 to 8 )
+    //and eight columns (denoted with letters a to h)
     private int row;
+ 
+    public static final int ROW_1 = 0;
+    public static final int ROW_2 = 1;
+    public static final int ROW_3 = 2;
+    public static final int ROW_4 = 3;
+    public static final int ROW_5 = 4;
+    public static final int ROW_6 = 5;
+    public static final int ROW_7 = 6;
+    public static final int ROW_8 = 7;
+ 
+    private int column;
+ 
+    public static final int COLUMN_A = 0;
+    public static final int COLUMN_B = 1;
+    public static final int COLUMN_C = 2;
+    public static final int COLUMN_D = 3;
+    public static final int COLUMN_E = 4;
+    public static final int COLUMN_F = 5;
+    public static final int COLUMN_G = 6;
+    public static final int COLUMN_H = 7;
+ 
     private boolean isCaptured = false;
     private Image img;
     private int x, y;
